a6ef8e8d21232f7efbdad93c9a26c59ae7a5f7bc
angelfernandezcic <angelfernandezcic@gmail.com>
2017-07-06 18:02:03 +0200

    Cambios Test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ejercicio06;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestConcatenar()
+        {
+
+            Metodo sut = new Metodo();
+
+            String concatenacion = sut.Concatenar();
+        }
+    }
+}
--- ejercicio06/Class1.cs
+++ ejercicio06/Class1.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ejercicio06
 {
-    public class Class1
+    public class Datos
     {
+        public String texto = "Hola";
+        public int a = 1;
+        public int b = 2;
+    }
+
+    public class Metodo : Datos
+    {
+
+        public String Concatenar()
+        {
+            return texto + a + b;
+        }
+        
     }
 }
